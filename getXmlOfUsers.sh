30615c5f5132349fba9daf91652b9bf6dad99e4b
HS <hsaghir@myges.fr>
2021-07-12 21:35:55 +0200

    Mise à jour des scripts

--- getXmlOfUsers.sh
+++ getXmlOfUsers.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
 
-echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-echo "<users>"
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > donneesUtilisateurs.xml 
+echo "<users>" >> donneesUtilisateurs.xml
 
 for i in $(cut -d: -f1,3,4,6,7 /etc/passwd)
 do
 	echo $i > /tmp/ligne.txt
-	
+
 	shell=$(cut -d: -f5 /tmp/ligne.txt)
-	
+
 	if [ $shell == "/bin/bash" ]
 	then	
 		name=$(cut -d: -f1 /tmp/ligne.txt)
 		uid=$(cut -d: -f2 /tmp/ligne.txt)
 		gid=$(cut -d: -f3 /tmp/ligne.txt)
 		home=$(cut -d: -f4 /tmp/ligne.txt)
-		
-		
 		taille=$(du -s $home | cut -f1)
-#		fingerprint= "A trouver"
-	
-		echo "	<user>"
-		echo "		<name>$name</name>"
-		echo "		<uid>$uid</uid>"
-		echo "		<gid>$gid</gid>"
-		echo "		<home>$home</home>"
-		echo "		<shell>$shell</shell>"
-		echo "		<taille>$taille</taille>"
-#		echo "		<fingerprint>$fingerprint</fingerprint>"
-		echo "		<fingerprint>A trouver</fingerprint>"
-		echo "	</user>"
+
+		# Sauvegarder le nom de l'archive et le créer
+		archiveName="/var/backups/$name.$(date +%m-%d-%y).$(date +%H-%M).tgz"
+		tar zcvf $archiveName $home
+
+		# Génerer le fingerprint de l'archive
+		fingerprint=$(md5sum $archiveName | cut -d" " -f1)
+
+        # Informations de l'utilisateur sous format XML
+		echo "	<user>" >> donneesUtilisateurs.xml
+		echo "		<name>$name</name>" >> donneesUtilisateurs.xml
+		echo "		<uid>$uid</uid>" >> donneesUtilisateurs.xml
+		echo "		<gid>$gid</gid>" >> donneesUtilisateurs.xml
+		echo "		<home>$home</home>" >> donneesUtilisateurs.xml
+		echo "		<shell>$shell</shell>" >> donneesUtilisateurs.xml
+		echo "		<taille>$taille</taille>" >> donneesUtilisateurs.xml
+		echo "		<fingerprint>$fingerprint</fingerprint>" >> donneesUtilisateurs.xml
+		echo "	</user>" >> donneesUtilisateurs.xml
 	fi
 done
-echo "</users>"
+echo "</users>" >> donneesUtilisateurs.xml
 
-rm /tmp/ligne.txt
