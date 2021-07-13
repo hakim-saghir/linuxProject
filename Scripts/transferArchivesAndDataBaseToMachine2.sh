135802b1b9c121d76eb88a224814191e13fdda20
Hakim SAGHIR <hsaghir@myges.fr>
2021-07-13 21:57:02 +0200

    mise à jour

--- Scripts/transferArchivesAndDataBaseToMachine2.sh
+++ Scripts/transferArchivesAndDataBaseToMachine2.sh
@@ -3,7 +3,8 @@
 echo "Entrez le nom de l'utilisateur de machine serveur, son adresse IP et le répertoire de sauvegarde sous ce format (utilisateur@addresseIP:RépertoireDeSauvegarde) : "
 read target
 
-for i in $(ls /var/backups/*.tgz || /var/backups/*.sql)
+for i in $(ls /var/backups/*.tgz /var/backups/*.sql)
 do
 	scp $i $target
+	
 done
--- Scripts/updateTailleFPAndTransferArchives.sh
+++ Scripts/updateTailleFPAndTransferArchives.sh
@@ -3,10 +3,13 @@
 # Affichage des statuts à la fin
 retour=""
 
-# Nettoyage des fichiers dans /var/backups/
-rm /var/backups/*.tgz
-rm /var/backups/*.sql
+# Des fichiers ont été modifiés
+transfer=false
 
+# Nettoyage des fichiers dans /var/backups/
+rm -f /var/backups/*.tgz /var/backups/*.sql
+
+# Parcourir les utilisateurs dans /etc/passwd
 for i in $(cut -d: -f1,3,4,6,7 /etc/passwd)
 do
 	echo $i > /tmp/ligne.txt
@@ -19,36 +22,50 @@ do
 		uid=$(cut -d: -f2 /tmp/ligne.txt)
 		gid=$(cut -d: -f3 /tmp/ligne.txt)
 		home=$(cut -d: -f4 /tmp/ligne.txt)
-		taille=$(du -s $home | cut -f1)
+		taille=$(sudo du -s $home | cut -f1)
 
-		selectedValues=$(mysql users_db -N -B -e "SELECT taille, fingerprint FROM user_tb WHERE uid = $uid")
+		selectedValues=$(sudo mysql users_db -N -B -e "SELECT taille, fingerprint FROM user_tb WHERE uid = $uid")
 
 		echo $selectedValues > /tmp/ligne.txt
 
 		# Sauvegarder le nom de l'archive et le créer
 		archiveName="/var/backups/$name.$(date +%m-%d-%y).$(date +%H-%M).tgz"
-		tar zcf $archiveName --absolute-names $home
+		sudo tar zcf $archiveName --absolute-names $home
 
 		# Génerer le fingerprint de l'archive
 		fingerprint=$(md5sum $archiveName | cut -d" " -f1)
 
+		# Si l'utilisateur est inexistant dans la table
 		if [ -z "$selectedValues" ]
 		then
 			retour="$retour\nUser: $name	|	taille: $taille	|	fingerprint: $fingerprint	[INSERTION]"
-			mysql users_db -e "INSERT INTO user_tb VALUES(0,'$name', $uid, $gid, '$home', '$shell', $taille, '$fingerprint')"
+			sudo mysql users_db -e "INSERT INTO user_tb VALUES(0,'$name', $uid, $gid, '$home', '$shell', $taille, '$fingerprint')"
+			transfer=true
 		else
 			old_taille=$(cut -d" " -f1 /tmp/ligne.txt)
 			old_fingerprint=$(cut -d" " -f2 /tmp/ligne.txt)
 
+			# Si la taille ou le fingerprint ont été  modifiés
 			if [ "$taille" != "$old_taille" ]  || [ "$fingerprint" != "$old_fingerprint" ];
 			then
 				retour="$retour\nUser: $name	|	taille: $taille	|	fingerprint: $fingerprint	[UPDATE]"
-				mysql users_db -e "UPDATE user_tb SET taille = $taille, fingerprint = '$fingerprint' WHERE uid = $uid"
+				sudo mysql users_db -e "UPDATE user_tb SET taille = $taille, fingerprint = '$fingerprint' WHERE uid = $uid"
+				transfer=true
+
+			# Rien n'a été modifié
 			else
 				retour="$retour\nUser: $name	|	taille: $taille	|	fingerprint: $fingerprint	[NO CHANGE]"
+				rm -f $archiveName
 			fi
 		fi
 	fi
 done
 
 echo -e $retour
+
+if $transfer
+then
+	sudo mysqldump users_db > /var/backups/save_db.sql
+	./transferArchivesAndDataBaseToMachine2.sh
+fi
+
