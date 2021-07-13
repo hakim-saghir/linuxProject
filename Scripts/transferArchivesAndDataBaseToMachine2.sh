a96fae83a2629fa2678fd0936ff3a60cab930c90
Hakim SAGHIR <hsaghir@myges.fr>
2021-07-13 17:03:08 +0200

    Mise à jour

---
+++ Scripts/transferArchivesAndDataBaseToMachine2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Entrez le nom de l'utilisateur de machine serveur, son adresse IP et le répertoire de sauvegarde sous ce format (utilisateur@addresseIP:RépertoireDeSauvegarde) : "
+read target
+
+for i in $(ls /var/backups/*.tgz)
+do
+	scp $i $target
+done
