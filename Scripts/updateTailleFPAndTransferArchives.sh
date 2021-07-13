15a7223ca366538569f5bb2e63ad1b049a1b78bc
Hakim SAGHIR <hsaghir@myges.fr>
2021-07-13 20:34:08 +0200

    Mise à jour

--- Scripts/updateTailleFPAndTransferArchives.sh
+++ Scripts/updateTailleFPAndTransferArchives.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Affichage des statuts à la fin
+retour=""
+
+# Nettoyage des fichiers dans /var/backups/
+rm /var/backups/*.tgz
+rm /var/backups/*.sql
 
 for i in $(cut -d: -f1,3,4,6,7 /etc/passwd)
 do
@@ -13,22 +19,22 @@ do
 		uid=$(cut -d: -f2 /tmp/ligne.txt)
 		gid=$(cut -d: -f3 /tmp/ligne.txt)
 		home=$(cut -d: -f4 /tmp/ligne.txt)
-		taille=$(sudo du -s $home | cut -f1)
+		taille=$(du -s $home | cut -f1)
 
-		selectedValues=$(sudo mysql users_db -N -B -e "SELECT taille, fingerprint FROM user_tb WHERE uid = $uid")
+		selectedValues=$(mysql users_db -N -B -e "SELECT taille, fingerprint FROM user_tb WHERE uid = $uid")
 
 		echo $selectedValues > /tmp/ligne.txt
 
 		# Sauvegarder le nom de l'archive et le créer
 		archiveName="/var/backups/$name.$(date +%m-%d-%y).$(date +%H-%M).tgz"
-		tar zcvf $archiveName $home > /tmp/archives.log
+		tar zcf $archiveName --absolute-names $home
 
 		# Génerer le fingerprint de l'archive
 		fingerprint=$(md5sum $archiveName | cut -d" " -f1)
 
 		if [ -z "$selectedValues" ]
 		then
-			echo "User :'$name' | taille: $taille | fingerprint: $fingerprint [INSERTION]"
+			retour="$retour\nUser: $name	|	taille: $taille	|	fingerprint: $fingerprint	[INSERTION]"
 			mysql users_db -e "INSERT INTO user_tb VALUES(0,'$name', $uid, $gid, '$home', '$shell', $taille, '$fingerprint')"
 		else
 			old_taille=$(cut -d" " -f1 /tmp/ligne.txt)
@@ -36,12 +42,13 @@ do
 
 			if [ "$taille" != "$old_taille" ]  || [ "$fingerprint" != "$old_fingerprint" ];
 			then
-				echo "User :'$name' | taille: $taille | fingerprint: $fingerprint [UPDATE]"
+				retour="$retour\nUser: $name	|	taille: $taille	|	fingerprint: $fingerprint	[UPDATE]"
 				mysql users_db -e "UPDATE user_tb SET taille = $taille, fingerprint = '$fingerprint' WHERE uid = $uid"
 			else
-				echo "User :'$name' | taille: $taille | fingerprint: $fingerprint [NO CHANGE]"
+				retour="$retour\nUser: $name	|	taille: $taille	|	fingerprint: $fingerprint	[NO CHANGE]"
 			fi
 		fi
 	fi
 done
 
+echo -e $retour
