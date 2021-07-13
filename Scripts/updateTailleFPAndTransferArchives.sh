7b66a9ec2c27a8663d6f1604c9c05a3cf636af9f
Hakim SAGHIR <hsaghir@myges.fr>
2021-07-13 19:09:25 +0200

    Correction des bugs

--- Scripts/updateTailleFPAndTransferArchives.sh
+++ Scripts/updateTailleFPAndTransferArchives.sh
@@ -8,33 +8,40 @@ do
 	shell=$(cut -d: -f5 /tmp/ligne.txt)
 
 	if [ $shell == "/bin/bash" ]
-	then	
+	then
 		name=$(cut -d: -f1 /tmp/ligne.txt)
 		uid=$(cut -d: -f2 /tmp/ligne.txt)
 		gid=$(cut -d: -f3 /tmp/ligne.txt)
 		home=$(cut -d: -f4 /tmp/ligne.txt)
-		taille=$(du -s $home | cut -f1)
-		
-		selectedValues=$(mysql users_db -N -B -e "SELECT taille, fingerprint FROM user_tb WHERE uid=$iud")
-		exit
+		taille=$(sudo du -s $home | cut -f1)
+
+		selectedValues=$(sudo mysql users_db -N -B -e "SELECT taille, fingerprint FROM user_tb WHERE uid = $uid")
+
 		echo $selectedValues > /tmp/ligne.txt
+
 		# Sauvegarder le nom de l'archive et le créer
 		archiveName="/var/backups/$name.$(date +%m-%d-%y).$(date +%H-%M).tgz"
-		tar zcvf $archiveName $home
+		tar zcvf $archiveName $home > /tmp/archives.log
 
 		# Génerer le fingerprint de l'archive
 		fingerprint=$(md5sum $archiveName | cut -d" " -f1)
+
 		if [ -z "$selectedValues" ]
 		then
-			mysqL users_db -e "INSERT INTO user_tb VALUES(0,'$name', $uid, $gid, '$home', '$shell', $taille, '$fingerprint')"
-		else	
-			old_taille=$(echo $selectedValues | cut -f1)
-			old_fingerprint=$(echo $selectedValues | cut -f2)
-			if [ $taille != $old_taille ] || [ $fingerprint != $old_fingerprint ];
+			echo "User :'$name' | taille: $taille | fingerprint: $fingerprint [INSERTION]"
+			mysql users_db -e "INSERT INTO user_tb VALUES(0,'$name', $uid, $gid, '$home', '$shell', $taille, '$fingerprint')"
+		else
+			old_taille=$(cut -d" " -f1 /tmp/ligne.txt)
+			old_fingerprint=$(cut -d" " -f2 /tmp/ligne.txt)
+
+			if [ "$taille" != "$old_taille" ]  || [ "$fingerprint" != "$old_fingerprint" ];
 			then
-				mysql users_db -e "UPDATE user_tb SET taille=$taille, fingerprint=$fingerprint WHERE uid=$uid" | echo "UPDATE : user :'$name' | taille: $taille | fingerprint: $fingerprint "
+				echo "User :'$name' | taille: $taille | fingerprint: $fingerprint [UPDATE]"
+				mysql users_db -e "UPDATE user_tb SET taille = $taille, fingerprint = '$fingerprint' WHERE uid = $uid"
+			else
+				echo "User :'$name' | taille: $taille | fingerprint: $fingerprint [NO CHANGE]"
 			fi
-		fi 
+		fi
 	fi
 done
 
