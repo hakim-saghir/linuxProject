ab893a36e65df5883ca8a5ed5e73f2b12ef1b2ee
hakim-saghir <73503150+hakim-saghir@users.noreply.github.com>
2021-07-26 07:57:46 +0200

    Add files via upload

---
+++ sql/save/users_db_save.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: users_db
+-- ------------------------------------------------------
+-- Server version	8.0.25-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_tb`
+--
+
+DROP TABLE IF EXISTS `user_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_tb` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `uid` int unsigned DEFAULT NULL,
+  `gid` int unsigned DEFAULT NULL,
+  `home` varchar(128) NOT NULL,
+  `shell` varchar(128) NOT NULL,
+  `taille` int unsigned NOT NULL,
+  `fingerprint` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `type` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_tb`
+--
+
+LOCK TABLES `user_tb` WRITE;
+/*!40000 ALTER TABLE `user_tb` DISABLE KEYS */;
+INSERT INTO `user_tb` VALUES (1,'root',0,0,'/root','/bin/bash',40,'52e3691111b09906a5511e7e8c309e37'),(2,'hs',1000,1000,'/home/hs','/bin/bash',157288,'a41a1c2a1780bace5db3d67605c85f62'),(3,'hs2',1001,1001,'/home/hs2','/bin/bash',16,'45a06c5aeeb37588cb939259b79f0b97'),(4,'hs3',1002,1002,'/home/hs3','/bin/bash',16,'0732e5d04df3845f620e41757f41c390'),(5,'hs4',1003,1003,'/home/hs4','/bin/bash',16,'f4f8b63cb68ffd79e9d49c13b93c15da');
+/*!40000 ALTER TABLE `user_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-26  6:47:13
